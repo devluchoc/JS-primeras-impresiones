64c02e8fa4073afb0aeff09a8ebea49b0fbdb240
Luciano Cancinos <lucianocancinos99@gmail.com>
2020-11-04 12:58:03 -0300

    Probando el JS

---
+++ tex.js
@@ -0,0 +1,7 @@
+var divPrueba = document.getElementById('prueba')
+divPrueba.innerHTML = `
+    <p> Esto es un parrafo </p>
+    <p> Esto es otro parrafo </p>
+    <a> Esto es un link </a>
+    <div> Esto es un nuevo div </div>
+`
